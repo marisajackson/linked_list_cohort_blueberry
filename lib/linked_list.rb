30efe0222dd27a66d6e5e2ee9280da8fd6e36cc2
MaRisa Jackson <mhljackson@gmail.com>
2014-07-14 09:18:01 -0500

    "Through test 11"

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,89 @@
 class LinkedList
 
+  attr_accessor :size
+  attr_accessor :payload
+
+  def initialize(item=nil, *other_items)
+    @size = 0
+    if item
+      push(item)
+    end
+
+    if other_items
+      other_items.each do |other_item|
+        push(other_item)
+      end
+    end
+  end
+
+  def push(value)
+    @size += 1
+    lli = LinkedListItem.new(value)
+    if @first_item
+      last_item.next_item = lli
+    else
+      @first_item = lli
+    end
+  end
+
+  def [](index)
+    get(index)
+  end
+
+  def []=(index, arg)
+    current_item = @first_item
+    index.times do
+      current_item = current_item.next_item
+    end
+
+      @payload = arg
+  end
+
+  def get(index)
+    raise IndexError if index < 0
+
+    current_item = @first_item
+    index.times do
+      if current_item
+        current_item = current_item.next_item
+      else
+        raise IndexError
+      end
+    end
+    current_item.payload
+  end
+
+  def last
+    if @first_item
+      last_item.payload
+    end
+  end
+
+  def to_s
+    if @size == 0
+      "| |"
+    else
+      list = []
+      current = 0
+      while current < @size
+        list << get(current)
+        current += 1
+      end
+      "| #{list.join(", ")} |"
+    end
+  end
+
+  private
+
+  def last_item
+    # if @first_item
+      current_item = @first_item
+      until current_item.last?
+        current_item = current_item.next_item
+      end
+      current_item
+    # end
+  end
+
+
 end
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,54 @@
 class LinkedListItem
   include Comparable
+
+  attr_reader :next_item
+  attr_reader :payload
+  # ALTERNATIVE
+  # def payload
+  #   @payload
+  # end
+
+  def initialize(value)
+    @payload = value
+  end
+
+  def next_item=(other_item)
+    raise ArgumentError if other_item === self
+    @next_item = other_item
+    # ALTERNATIVE
+    # if self == other_item
+    #   raise ArgumentError, "Can't set self as next item"
+    # else
+    #   @next_item = other_item
+    # end
+  end
+
+  def last?
+    @next_item.nil?
+    # ALTERNATIVE
+    # unless @next_item
+    #   true
+    # else
+    #   false
+    # end
+  end
+
+  def <=>(other_item)
+    if self.payload.class == other_item.payload.class
+      self.payload.to_s <=> other_item.payload.to_s
+    else
+      # int < string < symbol
+      precedence = [Fixnum, String, Symbol]
+      left = precedence.index(self.payload.class)
+      right = precedence.index(other_item.payload.class)
+      left <=> right
+    end
+
+  end
+
+  def ===(other_item)
+    self.equal? other_item
+  end
+
+
 end
