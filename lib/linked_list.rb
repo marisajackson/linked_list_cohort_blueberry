39994ab75d5f7475ab55b731748690edf049b648
MaRisa Jackson <mhljackson@gmail.com>
2014-07-14 20:01:33 -0500

    part 2

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,7 @@
 class LinkedList
 
   attr_accessor :size
-  attr_accessor :payload
+  attr_accessor :status
 
   def initialize(item=nil, *other_items)
     @size = 0
@@ -31,15 +31,34 @@ class LinkedList
   end
 
   def []=(index, arg)
-    current_item = @first_item
-    index.times do
-      current_item = current_item.next_item
-    end
-
-      @payload = arg
+    current_item = get(index, true)
+    current_item.payload = arg
   end
 
-  def get(index)
+  def delete(index)
+    @size -= 1
+    raise IndexError if index > size
+    if index == 0
+      @first_item = @first_item.next_item
+    else
+      previous_item = get(index - 1, true)
+      next_item = previous_item.next_item.next_item
+      previous_item.next_item = next_item
+    end
+  end
+
+  def index(arg)
+    current = 0
+    while current < @size
+      if get(current) == arg
+        return current
+      else
+        current += 1
+      end
+    end
+  end
+
+  def get(index, return_item=false)
     raise IndexError if index < 0
 
     current_item = @first_item
@@ -50,7 +69,44 @@ class LinkedList
         raise IndexError
       end
     end
-    current_item.payload
+    return_item ? current_item : current_item.payload
+  end
+
+  # def sorted?
+  #   current = 1
+  #   return true if @size < 2
+  #
+  #   while current < @size + 1
+  #     if current == @size
+  #       return @status
+  #     end
+  #     current_item = get(current, true)
+  #     previous_item = get(current - 1, true)
+  #
+  #     if current_item > previous_item || current_item == previous_item
+  #     # if previous_item <=> current_item
+  #       @status = true
+  #       current +=1
+  #     else
+  #       @status = false
+  #       return @status
+  #     end
+  #   end
+  # end
+
+  # Nathan's Method
+  def sorted?
+    return true if self.size <= 1
+    @size.times do |i|
+      current_item = get(i, true)
+
+      unless current_item.last?
+        results = current_item <=> current_item.next_item
+        if results == 1
+          return false
+        end
+      end
+    end
   end
 
   def last
@@ -76,13 +132,11 @@ class LinkedList
   private
 
   def last_item
-    # if @first_item
       current_item = @first_item
       until current_item.last?
         current_item = current_item.next_item
       end
       current_item
-    # end
   end
 
 
